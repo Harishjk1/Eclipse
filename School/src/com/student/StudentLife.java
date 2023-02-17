f5bf2b027be86555cb32c0665a569d68a5505190
Harish jk <harishjk405@gmail.com>
2023-02-17 10:34:52 +0530

    Eclipse Verify

---
+++ School/src/com/student/StudentLife.java
@@ -0,0 +1,36 @@
+package com.student;
+
+
+public class StudentLife {
+private  void studentDetails()
+	{
+byte age = 23;
+System.out.println("age :"+age);
+short mark = 31111;
+System.out.println("mark;"+mark);
+int pfno = 8888888;
+System.out.println("pfno"+pfno);
+long mobileno = 7777777777l;
+System.out.println("mobileno"+mobileno);
+float percentage = 23.77f;
+System.out.println("percentage"+percentage);
+double h =77.8898987777743;
+System.out.println("h:"+h);
+char initial='j';
+System.out.println("initial"+initial);
+String name ="harish";
+System.out.println("name:"+name);
+boolean jk = true;
+System.out.println("jk"+jk);
+
+}
+public static void main(String[] args) {
+	
+	StudentLife o = new StudentLife();
+	o.studentDetails();
+	
+	
+}
+}
+
+
